461781e09f7d53844ffeab70ba12864d82766c1b
AngelDown12 <angeldown12xz@gmail.com>
2025-07-19 20:48:19 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,133 +1,176 @@
+import { promises } from 'fs'
+import { join } from 'path'
+import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 
-const textCyberpunk = (text) => {
-  const charset = {
-    a: 'ᴀ', b: 'ʙ', c: 'ᴄ', d: 'ᴅ', e: 'ᴇ', f: 'ꜰ', g: 'ɢ',
-    h: 'ʜ', i: 'ɪ', j: 'ᴊ', k: 'ᴋ', l: 'ʟ', m: 'ᴍ', n: 'ɴ',
-    o: 'ᴏ', p: 'ᴘ', q: 'ǫ', r: 'ʀ', s: 'ꜱ', t: 'ᴛ', u: 'ᴜ',
-    v: 'ᴠ', w: 'ᴡ', x: 'x', y: 'ʏ', z: 'ᴢ'
-  }
-  return text.toLowerCase().split('').map(c => charset[c] || c).join('')
-}
-
 let tags = {
-  'main': textCyberpunk('sistema'),
-  'group': textCyberpunk('grupos'),
-  'serbot': textCyberpunk('sub bots'),
+'main': '𝘐𝘯𝘧𝘰 📚',
+'search': '𝘉𝘶𝘴𝘲𝘶𝘦𝘥𝘢𝘴 🔎',
+'game': '𝘑𝘶𝘦𝘨𝘰𝘴 🎮',
+'rpg': '𝘙𝘗𝘎 🌠',
+'rg': '𝘙𝘦𝘨𝘪𝘴𝘵𝘳𝘰 📁',
+'sticker': '𝘚𝘵𝘪𝘤𝘬𝘦𝘳𝘴 🏞',
+'img': '𝘐𝘮𝘢́𝘨𝘦𝘯𝘦𝘴 📸',
+'group': '𝘎𝘳𝘶𝘱𝘰𝘴 👥',
+'logo': '𝘓𝘰𝘨𝘰 - 𝘮𝘢𝘬𝘦𝘳 🎨',
+'nable': '𝘖𝘯 / 𝘖𝘧𝘧 📴',
+'downloader': '𝘋𝘦𝘴𝘤𝘢𝘳𝘨𝘢𝘴 📥',
+'tools': '𝘏𝘦𝘳𝘳𝘢𝘮𝘪𝘦𝘯𝘵𝘢𝘴 🔧',
+'fun': '𝘋𝘪𝘷𝘦𝘳𝘴𝘪𝘰́𝘯 🎲',
+'nsfw': '𝘕𝘴𝘧𝘸 🔞',
+'owner': '𝘊𝘳𝘦𝘢𝘥𝘰𝘳 😺',
+'audio': '𝘈𝘶𝘥𝘪𝘰𝘴 🔉',
+'advanced': '𝘈𝘷𝘢𝘯𝘻𝘢𝘥𝘰 💠',
+'freefire': '𝘍𝘳𝘦𝘦 𝘍𝘪𝘳𝘦 📌',
+'anime': '𝘈𝘯𝘪𝘮𝘦 🌸',
 }
 
 const defaultMenu = {
-  before: `⚠️ 𝗔𝗟𝗘𝗥𝗧𝗔 𝗗𝗘 𝗦𝗜𝗦𝗧𝗘𝗠𝗔 ⚠️
-┃ ⛧ 𝙸𝙽𝙸𝙲𝙸𝙰𝙽𝙳𝙾: 𝙱𝙻𝙲-𝚂𝚈𝚂.exe
-┃ ⛧ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾: %name
-┃ ⛧ 𝙼𝙾𝙳𝙾: %mode
-┃ ⛧ 𝙴𝚂𝚃𝙰𝙳𝙾:  𝗢𝗡𝗟𝗜𝗡𝗘 👻
-╚══⫷🔻𝙽𝙴𝚃𝚁𝚄𝙽𝙽𝙴𝚁🔻⫸══╝
+before: `
+꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷
 
-╭─[𝗘𝗦𝗧𝗔𝗗𝗢 𝗗𝗘 𝗨𝗦𝗨𝗔𝗥𝗜𝗢]─╮
-│ 📊 𝗡𝗜𝗩𝗘𝗟: %level
-│ ⚡ 𝗘𝗫𝗣: %exp / %maxexp
-│ 🧮 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦: %totalreg
-│ ⏱ 𝗧𝗜𝗘𝗠𝗣𝗢 𝗔𝗖𝗧𝗜𝗩𝗢: %muptime
-╰──────────────────╯
+🔥 𝗧𝗶𝗲𝗺𝗽𝗼 𝗔𝗰𝘁𝗶𝘃𝗼: 169 Horas
 
-🧬 𝗡𝗢𝗗𝗢 𝗛𝗔𝗖𝗞 𝗔𝗖𝗧𝗜𝗩𝗔𝗗𝗢
-✦ Elige un comando para ejecutar protocolo.
-✦ Operador: The Carlos 👑
+💻 𝗛𝗼𝘀𝘁𝗶𝗻𝗴 𝗔𝗰𝘁𝘂𝗮𝗹: 𝗦𝗸𝘆 𝘂𝗹𝘁𝗿𝗮
+
+🕷️ 𝗖𝗿𝗲𝗮𝗱𝗼𝗿: +5215565238431
 
 %readmore
+••••••••~
+
+\t\t\tB A R D O C K B O T
 `.trimStart(),
-
-  header: '\n╭─〔 🦠 %category 〕─╮',
-  body: '│ ⚙️ %cmd\n',
-  footer: '╰────────────────╯',
-  after: '\n⌬ 𝗖𝗬𝗕𝗘𝗥 𝗠𝗘𝗡𝗨 ☠️ - Sistema ejecutado con éxito.'
+header: '┣━━━ 〔 %category 〕━━━┫',
+body: '┃⋗ 🔥 %cmd\n',
+footer: '┗━━━━━━━━━━━━━━┛\n',
+after: '',
 }
 
-let handler = async (m, { conn, usedPrefix: _p }) => {
-  try {
-    // Evita que el mensaje se procese dos veces al pulsar botones
-    if (m.isButton || m.quoted?.fromMe) return
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
+try {
+let _package = JSON.parse(await promises.readFile(join(_dirname, '../package.json')).catch( => ({}))) || {}
+let { exp, limit, level } = global.db.data.users[m.sender]
+let { min, xp, max } = xpRange(level, global.multiplier)
+let name = await conn.getName(m.sender)
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, {
+day: 'numeric',
+month: 'long',
+year: 'numeric'
+})
+let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+day: 'numeric',
+month: 'long',
+year: 'numeric'
+}).format(d)
+let time = d.toLocaleTimeString(locale, {
+hour: 'numeric',
+minute: 'numeric',
+second: 'numeric'
+})
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) {
+process.send('uptime')
+_muptime = await new Promise(resolve => {
+process.once('message', resolve)
+setTimeout(resolve, 1000)
+}) * 1000
+}
+let muptime = clockString(_muptime)
+let uptime = clockString(_uptime)
+let totalreg = Object.keys(global.db.data.users).length
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+return {
+help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+prefix: 'customPrefix' in plugin,
+limit: plugin.limit,
+premium: plugin.premium,
+enabled: !plugin.disabled,
+}
+})
+for (let plugin of help)
+if (plugin && 'tags' in plugin)
+for (let tag of plugin.tags)
+if (!(tag in tags) && tag) tags[tag] = tag
+conn.menu = conn.menu ? conn.menu : {}
+let before = conn.menu.before || defaultMenu.before
+let header = conn.menu.header || defaultMenu.header
+let body = conn.menu.body || defaultMenu.body
+let footer = conn.menu.footer || defaultMenu.footer
+let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
+let _text = [
+before,
+...Object.keys(tags).map(tag => {
+return header.replace(/%category/g, tags[tag]) + '\n' + [
+...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+return menu.help.map(help => {
+return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+.replace(/%islimit/g, menu.limit ? '' : '')
+.replace(/%isPremium/g, menu.premium ? '' : '')
+.trim()
+}).join('\n')
+}),
+footer
+].join('\n')
+}),
+after
+].join('\n')
+let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+let replace = {
+'%': '%',
+p: _p, uptime, muptime,
+taguser: '@' + m.sender.split("@s.whatsapp.net")[0],
+wasp: '@0',
+me: conn.getName(conn.user.jid),
+npmname: _package.name,
+version: _package.version,
+npmdesc: _package.description,
+npmmain: _package.main,
+author: _package.author.name,
+license: _package.license,
+exp: exp - min,
+maxexp: xp,
+totalexp: exp,
+xp4levelup: max - exp,
+github: _package.homepage ? _package.homepage.url || package.homepage : '[unknown github url]',
+level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg,
+readmore: readMore
+}
+text = text.replace(new RegExp(%(${Object.keys(replace).sort((a, b) => b.length - a.length).join|}), 'g'), (, name) => '' + replace[name])
 
-    let { exp, level } = global.db.data.users[m.sender]
-    let { min, xp, max } = xpRange(level, global.multiplier)
-    let name = await conn.getName(m.sender)
-    let _uptime = process.uptime() * 1000
-    let muptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let mode = global.opts["self"] ? "Privado" : "Público"
+let pp = 'https://files.catbox.moe/gsyptn.mp4'  
+await conn.sendMessage(m.chat, {
 
-    let help = Object.values(global.plugins).filter(p => !p.disabled).map(p => ({
-      help: Array.isArray(p.help) ? p.help : [p.help],
-      tags: Array.isArray(p.tags) ? p.tags : [p.tags],
-      prefix: 'customPrefix' in p,
-      limit: p.limit,
-      premium: p.premium,
-      enabled: !p.disabled,
-    }))
+video: { url: 'https://files.catbox.moe/gsyptn.mp4' },
+caption: text.trim(),
+gifPlayback: true
+}, { quoted: m })
 
-    for (let plugin of help) {
-      if (plugin.tags) {
-        for (let t of plugin.tags) {
-          if (!(t in tags) && t) tags[t] = textCyberpunk(t)
-        }
-      }
-    }
-
-    const { before, header, body, footer, after } = defaultMenu
-
-    let _text = [
-      before,
-      ...Object.keys(tags).map(tag => {
-        const cmds = help
-          .filter(menu => menu.tags.includes(tag))
-          .map(menu => menu.help.map(cmd => body.replace(/%cmd/g, menu.prefix ? cmd : _p + cmd)).join('\n'))
-          .join('\n')
-        return `${header.replace(/%category/g, tags[tag])}\n${cmds}\n${footer}`
-      }),
-      after
-    ].join('\n')
-
-    let replace = {
-      '%': '%',
-      name,
-      level,
-      exp: exp - min,
-      maxexp: xp,
-      totalreg,
-      mode,
-      muptime,
-      readmore: String.fromCharCode(8206).repeat(4001)
-    }
-
-    let text = _text.replace(/%(\w+)/g, (_, key) => replace[key] || '')
-
-    await conn.sendMessage(m.chat, {
-      video: { url: 'https://files.catbox.moe/gsyptn.mp4' },
-      caption: text,
-      footer: '🧠 BLACK CLOVER SYSTEM ☘️',
-      buttons: [
-        { buttonId: `${_p}menurpg`, buttonText: { displayText: '🏛️ M E N U R P G' }, type: 1 },
-        { buttonId: `${_p}code`, buttonText: { displayText: '🕹 ＳＥＲＢＯＴ' }, type: 1 }
-      ],
-      viewOnce: true
-    }, { quoted: m })
-
-  } catch (e) {
-    console.error(e)
-    conn.reply(m.chat, '❎ Error al generar el menú del sistema.', m)
-  }
+} catch (e) {
+conn.reply(m.chat, 'Lo sentimos, el menú tiene un error.', m)
+throw e
+}
 }
 
-handler.help = ['menu', 'menú']
-handler.tags = ['main']
-handler.command = ['menu', 'menú', 'help', 'ayuda']
-handler.register = true
-export default handler
+handler.customPrefix = /^(menu|menú|ayuda|help)$/i;
+handler.command = new RegExp; // para que funcione sin prefijo
+handler.register = false;
+
+export default handler;
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
+
