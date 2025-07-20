780b95eb0fd3a924c2df9a6ecde4538508de072c
AngelDown12 <angeldown12xz@gmail.com>
2025-07-19 19:51:39 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -61,6 +61,6 @@ END:VCARD`.trim()
 
 handler.help = ['owner']
 handler.tags = ['owner']
-handler.command = /^owner$/i
+handler.command = /^\.owner$/i
 handler.register = false
 export default handler
