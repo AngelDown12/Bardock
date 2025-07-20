bba693e0a40e76722ce2dd3a95fa81233996ccd1
AngelDown12 <angeldown12xz@gmail.com>
2025-07-19 19:42:54 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -154,9 +154,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   caption: text.trim(),
   gifPlayback: true,
   buttons: [
-    { buttonId: '.owner', buttonText: { displayText: '👑 Owner' }, type: 1 },
-    { buttonId: '.comprar', buttonText: { displayText: '💰 Comprar Bot' }, type: 1 }
-  ],
+    buttons: [
+  { buttonId: 'owner', buttonText: { displayText: '👑 Owner' }, type: 1 },
+  { buttonId: 'comprar', buttonText: { displayText: '💰 Comprar Bot' }, type: 1 }
+],
   footer: '🍷 Shadow Bot - Menú Interactivo'
 }, { quoted: m })
 
