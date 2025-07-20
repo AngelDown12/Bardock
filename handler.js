9eae24cce3a94a4750ee7a20d0f9fcf4800523bc
AngelDown12 <angeldown12xz@gmail.com>
2025-07-19 22:15:31 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -465,9 +465,9 @@ console.error(e)
 
 global.dfail = (type, m, conn, usedPrefix) => {
         let msg = {
-        rowner: "🚫𝐄𝐬𝐭𝐞 𝐂𝐨𝐦𝐚𝐧𝐝𝐨 𝐬𝐨𝐥𝐨 𝐄𝐬 𝐏𝐚𝐫𝐚 𝐂𝐫𝐢𝐬𝐭𝐢𝐚𝐧 𝐌𝐢 𝐂𝐫𝐞𝐚𝐝𝐨𝐫.",
+        rowner: "```¡¡ ESTE COMANDO SOLO ES PARA (CRISTIAN) MI CREADOR```",
         owner: "```¡¡ ESTE COMANDO SOLO ES PARA (CRISTIAN) MI CREADOR```",
-        mods: "🪡𝐄𝐬𝐭𝐞 𝐂𝐨𝐦𝐚𝐧𝐝𝐨 𝐬𝐨𝐥𝐨 𝐄𝐬 𝐏𝐚𝐫𝐚 𝐂𝐫𝐢𝐬𝐭𝐢𝐚𝐧 𝐌𝐢 𝐂𝐫𝐞𝐚𝐝𝐨𝐫.",
+        mods: "```¡¡ ESTE COMANDO SOLO ES PARA (CRISTIAN) MI CREADOR```",
         premium: "🔑𝐍𝐨 𝐄𝐫𝐞𝐬 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐏𝐫𝐞𝐦𝐢𝐮𝐦.",
         group: "𝐂𝐨𝐦𝐚𝐧𝐝𝐨 𝐕𝐚𝐥𝐢𝐝𝐨 𝐒𝐨𝐥𝐨 𝐩𝐚𝐫𝐚 𝐆𝐫𝐮𝐩𝐨𝐬.",
         private: "💬𝐂𝐨𝐦𝐚𝐧𝐝𝐨 𝐒𝐨𝐥𝐨 𝐕𝐚𝐥𝐢𝐝𝐨 𝐩𝐚𝐫𝐚 𝐂𝐡𝐚𝐭 𝐩𝐫𝐢𝐯𝐚𝐝𝐨.",
